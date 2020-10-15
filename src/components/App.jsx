1edfb2bab69288c6e1b997328ef487bfe5cdff85
achilleleal <achilleleal@gmail.com>
2020-10-15 13:58:04 -0400

    created item card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,7 @@ import Layout from './Layout';
 function App() {
   return (
     <Layout>
-      <h1> Hello</h1>
-      <h1> Hello</h1>
-      <h1> Hello</h1>
-      <h1> Hello</h1>
+      
     </Layout>
   );
 }
