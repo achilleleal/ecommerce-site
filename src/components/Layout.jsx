2155eaaa4872d406ff3569eb78657f0a2ddcebc2
achilleleal <achilleleal@gmail.com>
2020-10-29 14:31:01 -0400

    restarted project from 0

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,48 @@
 import React from 'react'
 
-export default function Layout({ children }) {
+const Link = ({ to, onClick, image }) => {
+    return (
+        <li onClick={onClick} className="link">
+            <img src={image} className="link_image" alt=''/>
+            <h1>{to}</h1>
+        </li>
+    );
+}
+
+export default function Layout({ children, setRoute, logout }) {
     return (
         <>
             <header>
-                <h1 style={{margin: '10px auto'}}>FishMart</h1>
+                <nav>
+                    <ul className="nav-bar">
+                        <Link 
+                          to="ReactMart"
+                          image=""
+                          onClick={() => setRoute()}
+                        />
+                        <Link 
+                          to="Cart"
+                          image=""
+                          onClick={() => setRoute()}
+                        />
+                        <Link 
+                          to="Profile"
+                          image=""
+                          onClick={() => setRoute()}
+                        />
+                        <Link 
+                          to="Logout"
+                          image=""
+                          onClick={() => logout()}
+                        />
+                    </ul>
+                </nav>
             </header>
             <main>
                 {children}
             </main>
             <footer>
-                <p>Sebastián Leal. 2020.</p>
+                <p>Made with love by Sebastián Leal. 2020.</p>
             </footer>
         </>
     )
