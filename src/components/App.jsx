d3e0f0e9d246f8920dbc30186a60051a907c7dc0
achilleleal <achilleleal@gmail.com>
2020-10-29 14:31:17 -0400

    created Layout component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Layout from './Layout';
+import Layout from "./Layout";
+import ProductPage from './ProductPage';
 
 function App() {
   return (
     <Layout>
-      
+      <ProductPage />
     </Layout>
   );
 }
